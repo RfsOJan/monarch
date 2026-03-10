c6162ccd24a58fc4af4b8a14b608643181e1a98c
RfsOJan <rafaandrade2020s@gmail.com>
2026-03-10 20:08:06 -0300

    header pronto

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 //DOM JS
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+
     //Onjeto no HTML ícone de Sol ou Lua)
     const toggleTheme = document.getElementById('toggleTheme');
-    const rootHtml = documentElement;
+    const rootHtml = document.documentElement;
 
-//Função mudar/alternar a cor do tema dark/light)
+    //Função mudar/alternar a cor do tema dark/light)
 
-function changeTheme() {
-    //Tema atual
-    const currentTheme = rootHtml.getAttribute('data-theme');
-    const isDark = currentTheme === 'dark';
+    function changeTheme() {
+        //Tema atual
+        const currentTheme = rootHtml.getAttribute('data-theme');
+        const isDark = currentTheme === 'dark';
 
-    rootHtml.setAttribute('data-theme' , isDark ? 'light' : 'dark' );
+        rootHtml.setAttribute('data-theme', isDark ? 'light' : 'dark');
 
-    //troca do ícone
-    toggleTheme.classList.toggle('bi-sun' , !isDark);
-    toggleTheme.classList.toggle('bi-moon' , 'isDark')
-}
+        //troca do ícone
+        toggleTheme.classList.toggle('bi-sun', !isDark);
+        toggleTheme.classList.toggle('bi-moon', isDark);
+    }
 
-if (toggleTheme) {
-    toggleTheme.addEventListener('click' , changeTheme);
-}
+    if (toggleTheme) {
+        toggleTheme.addEventListener('click', changeTheme);
+    }
 
 });
